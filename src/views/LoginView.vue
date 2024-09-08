200aec1ba420a6f6468a8545a1bd58d4cecb1de7
Ching Huang <huangching.07@gmail.com>
2024-09-08 21:37:07 +0800

    modify layout

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -29,11 +29,12 @@ const signin = async () => {
   <div id="loginPage" class="bg-yellow">
     <div class="conatiner loginPage vhContainer">
       <div class="side">
-        <img
-          class="logoImg"
-          src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/todolist/logo.png"
-          alt=""
-        />
+        <a href="#">
+          <img
+            class="logoImg"
+            src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/todolist/logo.png"
+            alt=""
+        /></a>
         <img
           class="d-m-n"
           src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/todolist/img.png"
@@ -71,4 +72,3 @@ const signin = async () => {
     </div>
   </div>
 </template>
-<style scoped></style>
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -28,11 +28,12 @@ const signup = async () => {
   <div id="signUpPage" class="bg-yellow">
     <div class="conatiner signUpPage vhContainer">
       <div class="side">
-        <img
-          class="logoImg"
-          src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/todolist/logo.png"
-          alt=""
-        />
+        <a href="#">
+          <img
+            class="logoImg"
+            src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/todolist/logo.png"
+            alt=""
+        /></a>
         <img
           class="d-m-n"
           src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/todolist/img.png"
